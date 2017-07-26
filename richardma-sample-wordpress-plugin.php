0647b37e188258a49e9a4495a779a03fc7c0da9e
Richard Ma <richard.ma.19850509@gmail.com>
2017-07-26 21:02:13 +0800

    add google analytics href to admin bar

--- richardma-sample-wordpress-plugin.php
+++ richardma-sample-wordpress-plugin.php
@@ -8,3 +8,14 @@
  * Version: 0.1
  * Lincense: MIT
  */
+
+function rm_add_google_analytics() {
+    global $wp_admin_bar;
+    $wp_admin_bar->add_menu(array(
+        'id' => 'google_analytics',
+        'title' => 'Google Analytics',
+        'href' => 'http://google.com/analytics/'
+    ));
+}
+
+add_action('wp_before_admin_bar_render', 'rm_add_google_analytics');
