7e4b39387093ebab36a6c8713ce8c653de74ecb9
Richard Ma <richard.ma.19850509@gmail.com>
2017-07-26 17:51:47 +0800

    add a plugin file

---
+++ richardma-sample-wordpress-plugin.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Plugin Name: Richardma Sample Wordpress Plugin
+ * Plugin URI: https://github.com/richard-ma/richardma-sample-wordpress-plugin
+ * Author: Richard Ma
+ * Author URI: http://richardma.info
+ * Version: 0.1
+ * Lincense: MIT
+ */
