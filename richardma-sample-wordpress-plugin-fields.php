d41accf6bfab825ce3a15d47027fc4d8aa6c8142
Richard Ma <richard.ma.19850509@gmail.com>
2017-08-03 21:23:03 +0800

    add date listed

--- richardma-sample-wordpress-plugin-fields.php
+++ richardma-sample-wordpress-plugin-fields.php
@@ -8,7 +8,17 @@ function rm_meta_callback() {
                 <label for="job-id" class="rm-row-title">Job ID</label>
             </div>
             <div class="meta-td">
-                <input type="text" name="job-id" id="job-id" value="" />
+                <input type="text" class="rm-row-content" name="job-id" id="job-id" value="" />
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="meta-row">
+            <div class="meta-th">
+                <label for="date-listed" class="rm-row-title">Date Listed</label>
+            </div>
+            <div class="meta-td">
+                <input type="text" class="rm-row-content" name="date_listed" id="date-listed" value="" />
             </div>
         </div>
     </div>
