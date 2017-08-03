352a003a59c66b4678f18dbce0ac512b34aea244
Richard Ma <richard.ma.19850509@gmail.com>
2017-08-03 20:58:06 +0800

    add css and js files

--- richardma-sample-wordpress-plugin.php
+++ richardma-sample-wordpress-plugin.php
@@ -17,3 +17,14 @@ if (!defined('ABSPATH')) {
 // require other php file
 require_once( plugin_dir_path(__FILE__) . 'richardma-sample-wordpress-plugin-cpt.php');
 require_once( plugin_dir_path(__FILE__) . 'richardma-sample-wordpress-plugin-fields.php');
+
+function rm_admin_enqueue_scripts() {
+    global $pagenow, $typenow;
+
+    if (($pagenow == 'post.php' || $pagenow == 'post-new.php') && $typenow == 'job') {
+        wp_enqueue_style('rm-admin-css', plugins_url('css/admin-jobs.css', __FILE__));
+        wp_enqueue_script('rm-job-js', plugins_url('js/admin-jobs.js', __FILE__), array('jquery', 'jquery-ui-datepicker'), 20150204, true);
+    }
+}
+
+add_action('admin_enqueue_scripts', 'rm_admin_enqueue_scripts');
