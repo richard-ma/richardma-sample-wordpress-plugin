9e61d2258e379bd71a8f31748f22f95decf9fc8d
Richard Ma <richard.ma.19850509@gmail.com>
2017-08-04 14:31:51 +0800

    use loop show post from WP_Query

--- richardma-sample-wordpress-plugin-reorder-jobs.php
+++ richardma-sample-wordpress-plugin-reorder-jobs.php
@@ -16,16 +16,31 @@ add_action('admin_menu', 'rm_add_submenu_page');
 function reorder_admin_jobs_callback() {
 
     $args = array(
-        //'post_type' => 'job',
-        //'order_by' => 'menu_order',
-        //'order' => 'ASC',
-        //'no_found_rows' => true,
-        //'update_post_term_cache' => false,
-        //'post_per_page' => 50
-        'category_name' => 'articles'
+        'post_type' => 'job',
+        'order_by' => 'menu_order',
+        'order' => 'ASC',
+        'no_found_rows' => true,
+        'update_post_term_cache' => false,
+        'post_per_page' => 50
+        //'category_name' => 'articles'
     );
 
     $job_listing = new WP_Query($args);
-
-    var_dump($job_listing);
+?>
+    <div id="job-sort" class="wrap">
+        <div id="icon-job-admin" class="icon32"><br /></div>
+        <h2><?php _e('Sort Job Positions', 'wp-job-listing'); ?><img src="<?php echo esc_url(admin_url() . '/images/loading.gif'); ?>" id="loading-animation" /></h2>
+        <?php if ($job_listing->have_posts()) : ?>
+            <p><?php _e('<strong>Note:</strong>'); ?></p>
+            <ul id="custom-type-list">
+            <?php while ($job_listing->have_posts()) : $job_listing->the_post(); ?>
+                <li id="<?php the_id(); ?>"><?php the_title(); ?></li>
+            <?php endwhile; ?>
+            </ul>
+        <?php else: ?>
+            <p><?php _e('You have no jobs to sort.', 'wp-job-listing'); ?></p>
+        <?php endif; ?>
+    </div>
+<?php
+    //var_dump($job_listing->get_posts());
 }
