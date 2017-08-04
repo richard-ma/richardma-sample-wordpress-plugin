455177322271a963b219502050eb774b0c1ae1b3
Richard Ma <richard.ma.19850509@gmail.com>
2017-08-04 15:40:57 +0800

    ajax save sort order

--- js/reorder.js
+++ js/reorder.js
@@ -1,5 +1,28 @@
 jQuery(document).ready(function ($) {
     var sortList = $('ul#custom-type-list');
+    var animation = $('#loading-animation');
+    var pageTitle = $('div h2');
 
-    sortList.sortable({});
+    sortList.sortable({
+        update: function(event, ui) {
+            animation.show();
+
+            $.ajax({
+                url: ajaxurl,
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    action: 'save_post',
+                    order: sortList.sortable('toArray').toString()
+                },
+                success: function(response) {
+                    animation.hide(); // last thing
+                    pageTitle.after('<div class="updated"><p>Jobs sort order has been saved.</p></div>');
+                },
+                error: function(error) {
+                    pageTitle.after('<div class="error"><p>There was an error while saving jobs sort order.</p></div>');
+                }
+            });
+        }
+    });
 });
